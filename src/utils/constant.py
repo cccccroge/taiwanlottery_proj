ee29c4ee0e6272882e23e35dc5dd4446c63d2dc2
cccccroge <pooopooo543@gmail.com>
2022-01-31 13:05:15 +0800

    Add big lottery

--- src/utils/constant.py
+++ src/utils/constant.py
@@ -1,9 +1,14 @@
-from scrapy_proj.scrapy_proj.spiders import gintsai539_spider
+from scrapy_proj.scrapy_proj.spiders import gintsai539_spider, big_lottery_spider
 
 GAMES = {
     'gin': {
         'key': 'gintsai539',
         'spider': gintsai539_spider.Gintsai539Spider,
         'label': '今彩539',
+    },
+    'big': {
+        'key': 'big_lottery',
+        'spider': big_lottery_spider.BigLotterySpider,
+        'label': '大樂透',
     },
 }
