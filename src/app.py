0c4e45e7f2d4cb65596bb8ec867d8081a2a0b738
cccccroge <pooopooo543@gmail.com>
2022-02-03 22:57:34 +0800

    Add kivymd, implement screens

--- src/app.py
+++ src/app.py
@@ -3,26 +3,59 @@ from utils.paths import ASSET_FOLDER, KV_FOLDER
 from utils.constant import Game
 from crawler import Crawler
 import kivy
-
-kivy.require("2.0.0")
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
+from kivymd.app import MDApp
+from kivymd.uix.button import MDFillRoundFlatButton
+from kivymd.uix.behaviors.toggle_behavior import MDToggleButton
 from kivy.lang.builder import Builder
+from kivy.core.window import Window
+from kivy.modules import inspector
+from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.anchorlayout import AnchorLayout
 import os.path
 
 
-class RootWidget(BoxLayout):
+class RootWidget(ScreenManager):
     pass
 
 
-class MyApp(App):
+class NextStep(AnchorLayout):
+    def slide_to_next(self):
+        sm = MDApp.get_running_app().root
+        names = sm.screen_names
+        idx = names.index(sm.current_screen.name)
+        next_idx = (idx + 1) % len(names)
+        sm.current = names[next_idx]
+
+
+class GameButton(MDFillRoundFlatButton, MDToggleButton):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.background_down = self.theme_cls.primary_light
+
+
+class MyApp(MDApp):
     def build(self):
+        # set title
         self.title = "台灣彩券分析"
+
+        # assign asset path
         kivy.resources.resource_add_path(ASSET_FOLDER)
-        Builder.load_file(os.path.join(KV_FOLDER, "step_layout.kv"))
+
+        # assign kv path
+        Builder.load_file(os.path.join(KV_FOLDER, "layout.kv"))
+        Builder.load_file(os.path.join(KV_FOLDER, "choose_game.kv"))
+        Builder.load_file(os.path.join(KV_FOLDER, "choose_range.kv"))
         Builder.load_file(os.path.join(KV_FOLDER, "root.kv"))
 
-        return RootWidget()
+        # dark theme
+        self.theme_cls.theme_style = "Dark"
+
+        root = RootWidget()
+
+        # debug mode
+        inspector.create_inspector(Window, root)
+
+        return root
 
 
 def main():
